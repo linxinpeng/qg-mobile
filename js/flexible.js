12cc82f7b4792cc8c583c456e7e4e943bd46a745
linxinpeng <554085340@qq.com>
2020-08-30 13:09:13 +0800

    登录界面跟注册界面

--- js/flexible.js
+++ js/flexible.js
@@ -1,49 +1,24 @@
-//designWidth:设计稿的实际宽度值，需要根据实际设置
-//maxWidth:制作稿的最大宽度值，需要根据实际设置
-//这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
-;(function(designWidth, maxWidth) {
-    var doc = document,
-    win = window,
-    docEl = doc.documentElement,
-    remStyle = document.createElement("style"),
-    tid;
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        isIOS = navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+        dpr = isIOS ? Math.min(win.devicePixelRatio, 3) : 1,
+        dpr = window.top === window.self ? dpr : 1, //被iframe引用时，禁止缩放
+        scale = 1 / dpr,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    docEl.dataset.dpr = dpr;
+    var metaEl = doc.querySelector("meta[name='viewport']");
+    metaEl.content = 'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale;
 
-    function refreshRem() {
-        var width = docEl.getBoundingClientRect().width;
-        maxWidth = maxWidth || 540;
-        width>maxWidth && (width=maxWidth);
-        var rem = width * 100 / designWidth;
-        remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
-    }
-
-    if (docEl.firstElementChild) {
-        docEl.firstElementChild.appendChild(remStyle);
-    } else {
-        var wrap = doc.createElement("div");
-        wrap.appendChild(remStyle);
-        doc.write(wrap.innerHTML);
-        wrap = null;
-    }
-    //要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
-    refreshRem();
-
-    win.addEventListener("resize", function() {
-        clearTimeout(tid); //防止执行两次
-        tid = setTimeout(refreshRem, 300);
-    }, false);
-
-    win.addEventListener("pageshow", function(e) {
-        if (e.persisted) { // 浏览器后退的时候重新计算
-            clearTimeout(tid);
-            tid = setTimeout(refreshRem, 300);
+    var recalc = function() {
+        var width = docEl.clientWidth;
+        if (width / dpr > 750) {//750为设计稿的宽度，此处应根据实际情况改变
+            width = 750 * dpr;
         }
-    }, false);
+        // 乘以100，px : rem = 100 : 1
+        docEl.style.fontSize = 100 * (width / 750) + 'px';
+    };
 
-    if (doc.readyState === "complete") {
-        doc.body.style.fontSize = "16px";
-    } else {
-        doc.addEventListener("DOMContentLoaded", function(e) {
-            doc.body.style.fontSize = "16px";
-        }, false);
-    }
-})(750, 750);
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
